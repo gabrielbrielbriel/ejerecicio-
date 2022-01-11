7292243a8892bc38e1cc67104b4cabfe77c5d6a2
gabrielbrielbriel <93211645+gabrielbrielbriel@users.noreply.github.com>
2022-01-11 18:18:21 +0100

    Add files via upload

---
+++ escripito.js
@@ -0,0 +1,10 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+alert("😸");/*Vai entre comillas */
+alert("2+2");
+alert(146*567);/*Non vai entre comillas*/
+ 
+ 
